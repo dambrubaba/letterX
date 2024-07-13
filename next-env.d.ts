776a48c1200791825509a3af53ae2faac9508364
Damburudhar Reddy <166049920+dambrubaba@users.noreply.github.com>
2024-07-13 19:17:53 +0000

    issuer base uri bug 2

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,16 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference types="next-auth" />
+/// <reference types="next-auth/jwt" />
 
+declare namespace NodeJS {
+  interface ProcessEnv {
+    AUTH0_SECRET: string;
+    AUTH0_BASE_URL: string;
+    AUTH0_ISSUER_BASE_URL: string;
+    AUTH0_CLIENT_ID: string;
+    AUTH0_CLIENT_SECRET: string;
+  }
+}
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
