879d3ccba6b1c90e2487b584988f2ec04e877420
Damburudhar Reddy <166049920+dambrubaba@users.noreply.github.com>
2024-07-16 19:44:43 +0000

    auth0 myapp.local

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = {
-  reactStrictMode: true,
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "myapp.local-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "myapp.local.pem")),
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
 };
